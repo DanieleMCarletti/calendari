f7cb762d2f747e68ad6e506fbd3a18684f511c55
DanieleMCarletti <116156321+DanieleMCarletti@users.noreply.github.com>
2025-05-30 13:14:19 +0200

    Create eventi_2025_08.py

---
+++ dati_grezzi/eventi_2025_08.py
@@ -0,0 +1,23 @@
+# dati_grezzi/eventi_2025_08.py
+event_list = [
+    {
+        'summary': 'BLACKPINK - BORN PINK World Tour ENCORE', # Aggiunto nome tour
+        'dtstart_str': '2025-08-06T20:30:00', # Orario ipotizzato
+        'dtend_str': '2025-08-06T23:00:00',   # Orario ipotizzato
+        'location_name': 'Ippodromo SNAI La Maura',
+        'location_address': 'Via Lampugnano, 95, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+La+Maura%2C+Via+Lampugnano%2C+95%2C+20151+Milano+MI%2C+Italy',
+        'description': "Concerto delle BLACKPINK."
+    },
+    {
+        'summary': 'Post Malone - I-Days Milano',
+        'dtstart_str': '2025-08-27T20:30:00', # Orario ipotizzato
+        'dtend_str': '2025-08-27T23:00:00',   # Orario ipotizzato
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Concerto di Post Malone, parte degli I-Days Milano Coca-Cola."
+    }
+    # Agosto è spesso più tranquillo per i grandissimi eventi negli stadi/ippodromi principali
+    # a causa delle ferie estive, ma potrebbero esserci eventi minori o annunci successivi.
+]
