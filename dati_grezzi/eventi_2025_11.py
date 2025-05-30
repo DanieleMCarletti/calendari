a63acd331d7b4f7927b759eb06c279316959025b
DanieleMCarletti <116156321+DanieleMCarletti@users.noreply.github.com>
2025-05-30 14:35:27 +0200

    Create eventi_2025_11.py

---
+++ dati_grezzi/eventi_2025_11.py
@@ -0,0 +1,22 @@
+# dati_grezzi/eventi_2025_11.py
+event_list = [
+    # Eventi Ippici (se di interesse)
+    {
+        'summary': 'Premio Vittorio di Capua',
+        'dtstart_str': '2025-11-09T14:00:00', # Orario tipico
+        'dtend_str': '2025-11-09T18:00:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Giornata di corse ippiche con il prestigioso Premio Vittorio di Capua."
+    },
+    {
+        'summary': 'Premio Federico Tesio – St. Leger Italiano',
+        'dtstart_str': '2025-11-16T14:00:00', # Orario tipico
+        'dtend_str': '2025-11-16T18:00:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Giornata di corse ippiche con i premi Federico Tesio e St. Leger Italiano."
+    }
+]
