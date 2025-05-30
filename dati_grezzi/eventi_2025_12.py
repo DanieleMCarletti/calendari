8e99db22766e16b2c49287202d408d3165252f09
DanieleMCarletti <116156321+DanieleMCarletti@users.noreply.github.com>
2025-05-30 14:37:10 +0200

    Create eventi_2025_12.py

---
+++ dati_grezzi/eventi_2025_12.py
@@ -0,0 +1,31 @@
+# dati_grezzi/eventi_2025_12.py
+event_list = [
+    # Eventi Ippici (se di interesse, spesso ci sono corse fino a fine anno)
+    {
+        'summary': 'Giornata di corse ippiche (Dicembre 1)',
+        'dtstart_str': '2025-12-07T13:30:00', # Orario tipico invernale
+        'dtend_str': '2025-12-07T17:00:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Corse ippiche all'Ippodromo SNAI San Siro."
+    },
+    {
+        'summary': 'Giornata di corse ippiche (Dicembre 2)',
+        'dtstart_str': '2025-12-13T13:30:00', # Orario tipico invernale
+        'dtend_str': '2025-12-13T17:00:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Corse ippiche all'Ippodromo SNAI San Siro."
+    },
+    {
+        'summary': 'Giornata di corse ippiche (Dicembre 3 - Santo Stefano)',
+        'dtstart_str': '2025-12-26T13:30:00', # Orario tipico invernale
+        'dtend_str': '2025-12-26T17:00:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Tradizionali corse ippiche di Santo Stefano."
+    }
+]
