541f48c9e1e3b605575e93b048d280175919c5f4
DanieleMCarletti <116156321+DanieleMCarletti@users.noreply.github.com>
2025-05-30 13:14:39 +0200

    Create eventi_2025_09.py

---
+++ dati_grezzi/eventi_2025_09.py
@@ -0,0 +1,41 @@
+# dati_grezzi/eventi_2025_09.py
+event_list = [
+    {
+        'summary': 'Lucio Corsi - Ippodromi 2025',
+        'dtstart_str': '2025-09-07T21:00:00', # Orario ipotizzato
+        'dtend_str': '2025-09-07T23:00:00',   # Orario ipotizzato
+        'location_name': 'Ippodromo SNAI San Siro', # O potrebbe essere La Maura, i tour "Ippodromi" a volte variano
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Concerto di Lucio Corsi."
+    },
+    {
+        'summary': 'Olly - La Grande Festa (Data 1)', # Ipotizzando nomi e date basati su info frammentarie
+        'dtstart_str': '2025-09-02T21:00:00',
+        'dtend_str': '2025-09-02T23:00:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Concerto di Olly."
+    },
+    {
+        'summary': 'Olly - La Grande Festa (Data 2)',
+        'dtstart_str': '2025-09-04T21:00:00',
+        'dtend_str': '2025-09-04T23:00:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Replica concerto di Olly."
+    },
+    {
+        'summary': 'Tananai - Calmocobra Live Estate 2025',
+        'dtstart_str': '2025-09-05T21:00:00',
+        'dtend_str': '2025-09-05T23:30:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport, 16, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+SNAI+San+Siro%2C+Piazzale+dello+Sport%2C+16%2C+20151+Milano+MI%2C+Italy',
+        'description': "Concerto di Tananai."
+    }
+    # Settembre potrebbe vedere anche l'inizio della stagione calcistica,
+    # quindi i feed di Inter/Milan inizieranno a popolarsi se non lo sono già.
+]
