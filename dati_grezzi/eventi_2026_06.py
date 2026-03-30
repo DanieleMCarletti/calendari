08ef9074a351dcfe42b8bf6dfcad37e94e2de1d6
DanieleMCarletti <116156321+DanieleMCarletti@users.noreply.github.com>
2026-03-30 09:47:23 +0200

    Create eventi_2026_06.py
    Aggiunto file per eventi giugno 2026

---
+++ dati_grezzi/eventi_2026_06.py
@@ -0,0 +1,110 @@
+event_list = [
+    {
+        'summary': 'Tiziano Ferro',
+        'dtstart_str': '2026-06-06T21:00:00',
+        'dtend_str': '2026-06-06T23:30:00',
+        'location_name': 'Stadio San Siro (Giuseppe Meazza)',
+        'location_address': 'Piazzale Angelo Moratti, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Stadio+San+Siro+Milano',
+        'description': 'Concerto di Tiziano Ferro allo Stadio San Siro.'
+    },
+    {
+        'summary': 'Tiziano Ferro',
+        'dtstart_str': '2026-06-07T21:00:00',
+        'dtend_str': '2026-06-07T23:30:00',
+        'location_name': 'Stadio San Siro (Giuseppe Meazza)',
+        'location_address': 'Piazzale Angelo Moratti, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Stadio+San+Siro+Milano',
+        'description': 'Concerto di Tiziano Ferro allo Stadio San Siro (2o giorno).'
+    },
+    {
+        'summary': 'Eros Ramazzotti',
+        'dtstart_str': '2026-06-09T21:00:00',
+        'dtend_str': '2026-06-09T23:30:00',
+        'location_name': 'Stadio San Siro (Giuseppe Meazza)',
+        'location_address': 'Piazzale Angelo Moratti, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Stadio+San+Siro+Milano',
+        'description': 'Concerto di Eros Ramazzotti.'
+    },
+    {
+        'summary': 'Cesare Cremonini',
+        'dtstart_str': '2026-06-10T21:00:00',
+        'dtend_str': '2026-06-10T23:30:00',
+        'location_name': 'Ippodromo SNAI La Maura',
+        'location_address': 'Via Lampugnano 95, Milano',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+La+Maura+Milano',
+        'description': 'Concerto di Cesare Cremonini (area ippodromo).'
+    },
+    {
+        'summary': 'Irama',
+        'dtstart_str': '2026-06-11T21:00:00',
+        'dtend_str': '2026-06-11T23:30:00',
+        'location_name': 'Stadio San Siro (Giuseppe Meazza)',
+        'location_address': 'Piazzale Angelo Moratti, 20151 Milano MI, Italy',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Stadio+San+Siro+Milano',
+        'description': 'Concerto di Irama.'
+    },
+    {
+        'summary': 'Geolier',
+        'dtstart_str': '2026-06-13T21:00:00',
+        'dtend_str': '2026-06-13T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=San+Siro',
+        'description': 'Concerto Geolier.'
+    },
+    {
+        'summary': 'Achille Lauro',
+        'dtstart_str': '2026-06-13T21:00:00',
+        'dtend_str': '2026-06-13T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=San+Siro',
+        'description': 'Concerto Achille Lauro.'
+    },
+    {
+        'summary': 'Iron Maiden',
+        'dtstart_str': '2026-06-13T21:00:00',
+        'dtend_str': '2026-06-13T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=San+Siro',
+        'description': 'Concerto Iron Maiden.'
+    },
+    {
+        'summary': 'Ligabue',
+        'dtstart_str': '2026-06-13T21:00:00',
+        'dtend_str': '2026-06-13T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=San+Siro',
+        'description': 'Concerto Ligabue.'
+    },
+    {
+        'summary': 'Tedua',
+        'dtstart_str': '2026-06-24T21:00:00',
+        'dtend_str': '2026-06-24T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=San+Siro',
+        'description': 'Concerto Tedua.'
+    },
+    {
+        'summary': 'Maroon 5',
+        'dtstart_str': '2026-06-25T21:00:00',
+        'dtend_str': '2026-06-25T23:30:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Piazzale dello Sport 16, Milano',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=Ippodromo+San+Siro',
+        'description': 'I-Days Milano.'
+    },
+    {
+        'summary': 'Gabri Ponte',
+        'dtstart_str': '2026-06-27T21:00:00',
+        'dtend_str': '2026-06-27T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://www.google.com/maps/search/?api=1&query=San+Siro',
+        'description': 'Concerto Gabri Ponte.'
+    }
+]
