0ce3a8b67e9a097e9a22a8e027d5bfd8678e224e
DanieleMCarletti <116156321+DanieleMCarletti@users.noreply.github.com>
2026-03-30 09:48:07 +0200

    Aggiunto file eventi 07/2026
    Aggiunto file eventi 07/2026

---
+++ dati_grezzi/eventi_2026_07.py
@@ -0,0 +1,92 @@
+event_list = [
+    {
+        'summary': 'Florence + The Machine',
+        'dtstart_str': '2026-07-03T21:00:00',
+        'dtend_str': '2026-07-03T23:30:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=Ippodromo+San+Siro',
+        'description': 'I-Days Milano.'
+    },
+    {
+        'summary': 'Foo Fighters',
+        'dtstart_str': '2026-07-05T21:00:00',
+        'dtend_str': '2026-07-05T23:30:00',
+        'location_name': 'Ippodromo SNAI La Maura',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=Ippodromo+La+Maura',
+        'description': 'I-Days Milano.'
+    },
+    {
+        'summary': 'System of a Down',
+        'dtstart_str': '2026-07-06T21:00:00',
+        'dtend_str': '2026-07-06T23:30:00',
+        'location_name': 'Ippodromo SNAI La Maura',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=Ippodromo+La+Maura',
+        'description': 'I-Days Milano.'
+    },
+    {
+        'summary': 'Sfera Ebbasta',
+        'dtstart_str': '2026-07-08T21:00:00',
+        'dtend_str': '2026-07-08T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=San+Siro',
+        'description': 'Prima data.'
+    },
+    {
+        'summary': 'Sfera Ebbasta',
+        'dtstart_str': '2026-07-09T21:00:00',
+        'dtend_str': '2026-07-09T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=San+Siro',
+        'description': 'Seconda data.'
+    },
+    {
+        'summary': 'Bad Bunny',
+        'dtstart_str': '2026-07-17T21:00:00',
+        'dtend_str': '2026-07-17T23:30:00',
+        'location_name': 'Ippodromo SNAI La Maura',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=La+Maura',
+        'description': 'Tour europeo.'
+    },
+    {
+        'summary': 'Bad Bunny',
+        'dtstart_str': '2026-07-18T21:00:00',
+        'dtend_str': '2026-07-18T23:30:00',
+        'location_name': 'Ippodromo SNAI La Maura',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=La+Maura',
+        'description': 'Seconda data.'
+    },
+    {
+        'summary': 'The Weeknd',
+        'dtstart_str': '2026-07-24T21:00:00',
+        'dtend_str': '2026-07-24T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=San+Siro',
+        'description': 'Tour mondiale.'
+    },
+    {
+        'summary': 'The Weeknd',
+        'dtstart_str': '2026-07-25T21:00:00',
+        'dtend_str': '2026-07-25T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=San+Siro',
+        'description': 'Seconda data.'
+    },
+    {
+        'summary': 'The Weeknd',
+        'dtstart_str': '2026-07-26T21:00:00',
+        'dtend_str': '2026-07-26T23:30:00',
+        'location_name': 'Stadio San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=San+Siro',
+        'description': 'Terza data.'
+    }
+]
