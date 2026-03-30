2a7cdeb3b982f8362af22f47c39dcf177ce93ce1
DanieleMCarletti <116156321+DanieleMCarletti@users.noreply.github.com>
2026-03-30 09:48:40 +0200

    Aggiunto file eventi 09/2026
    Aggiunto file eventi 09/2026

---
+++ dati_grezzi/eventi_2026_09.py
@@ -0,0 +1,56 @@
+event_list = [
+    {
+        'summary': 'Tony Pitony',
+        'dtstart_str': '2026-09-04T21:00:00',
+        'dtend_str': '2026-09-04T23:30:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=Ippodromo+San+Siro',
+        'description': 'I-Days Milano.'
+    },
+    {
+        'summary': 'David Guetta',
+        'dtstart_str': '2026-09-06T21:00:00',
+        'dtend_str': '2026-09-06T23:30:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=Ippodromo+San+Siro',
+        'description': 'DJ set.'
+    },
+    {
+        'summary': 'Emma Marrone',
+        'dtstart_str': '2026-09-09T21:00:00',
+        'dtend_str': '2026-09-09T23:30:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=Ippodromo+San+Siro',
+        'description': 'Concerto live.'
+    },
+    {
+        'summary': 'A$AP Rocky',
+        'dtstart_str': '2026-09-10T21:00:00',
+        'dtend_str': '2026-09-10T23:30:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=Ippodromo+San+Siro',
+        'description': 'I-Days Milano.'
+    },
+    {
+        'summary': 'Marracash & Gué',
+        'dtstart_str': '2026-09-12T21:00:00',
+        'dtend_str': '2026-09-12T23:30:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=Ippodromo+San+Siro',
+        'description': 'Prima data.'
+    },
+    {
+        'summary': 'Marracash & Gué',
+        'dtstart_str': '2026-09-13T21:00:00',
+        'dtend_str': '2026-09-13T23:30:00',
+        'location_name': 'Ippodromo SNAI San Siro',
+        'location_address': 'Milano',
+        'google_maps_url_str': 'https://maps.google.com/?q=Ippodromo+San+Siro',
+        'description': 'Seconda data.'
+    }
+]
